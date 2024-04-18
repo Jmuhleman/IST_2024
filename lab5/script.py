becdfd5ab07879bddb16e160d0614a0c3cf8b6ec
Jmuhleman <julien.muhlemann@heig-vd.ch>
2024-04-18 17:47:13 +0200

    add script down/upload data on s3

---
+++ lab5/script.py
@@ -0,0 +1,33 @@
+import requests
+import logging
+import boto3
+import os
+from botocore.exceptions import ClientError
+
+def download(url):
+    r = requests.get(url)
+    return r.text
+
+def upload_to_s3(data, bucket, object_name):
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.put_object(Body=data, Bucket=bucket, Key=object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+# Download CSV data
+data = download("https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA80.csv")
+
+# Upload data to S3 bucket
+bucket_name = "ist-grb-muhlemann-test"
+object_name = "VQHA80.csv"
+
+if upload_to_s3(data, bucket_name, object_name):
+    print("File uploaded successfully to S3 bucket:", bucket_name)
+else:
+    print("Failed to upload file to S3 bucket")
+
+
+
